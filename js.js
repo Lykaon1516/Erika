a896d64ded69b213de59d6cdeaa4fe40577fcb3f
luis rios <wolfblack201201@gmail.com>
2026-02-27 22:07:12 -0600

    Primer commit: archivos de cumpleaños

---
+++ js.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById('btnIngresar');
+
+    boton.addEventListener('click', () => {
+        alert('Iniciando sesión...');
+        // Aquí puedes poner: window.location.href = "otra_pagina.html";
+    });
+});
